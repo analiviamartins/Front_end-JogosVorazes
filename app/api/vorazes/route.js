98a4e6a6eab5aec5988365b26a805f5e8c6fcfaa
analiviamartins <anamartins.senai564@gmail.com>
2023-11-23 10:14:47 -0300

    first commit

---
+++ app/api/vorazes/route.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+import { NextResponse } from "next/server";
+
+const url = process.env.BASE_URL;
+
+export async function GET() {
+  try {
+    const resposta = await axios.get(url);
+
+    return NextResponse.json(resposta.data);
+  } catch (error) {
+    console.log("[ORDER_GET]", error);
+    return new NextResponse("Erro interno do servidor!", { status: 500 });
+  }
+}
+
+export async function POST(request) {
+  const params = await request.json();
+
+  try {
+    const resposta = await axios.post(url, params);
+
+    return NextResponse.json(resposta.data);
+  } catch (error) {
+    console.log("[ORDER_POST]", error);
+    return new NextResponse("Erro interno do servidor!", { status: 500 });
+  }
+}
