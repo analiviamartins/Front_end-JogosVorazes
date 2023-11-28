f964baefe07b277778d71a8419ec813a041bc7da
PedrooSantoss <pedromiguel.senai564@gmail.com>
2023-11-28 09:04:48 -0300

    feat: primeira parte index2

--- app/page.jsx
+++ app/page.jsx
@@ -4,14 +4,16 @@ import VideoPlayer from '@/src/VideoPlayer';
 
 function App() {
   return (
-    <>
+    <div>
     <div className='escuro'>
       <div className={styles.container}>
-      <img src="/banner.jpg" alt="Banner" className={styles.banner} width={1280} height={600}/>
+      <img src="/banner.jpg" alt="Banner" className={styles.banner} width={1270} height={600}/>
       </div>
+      <div className={styles.video}>
       <VideoPlayer />
       </div>
-    </>
+      </div>
+    </div>
   );
 };
 
