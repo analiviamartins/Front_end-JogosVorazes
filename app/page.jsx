8b980adfe49eccd034c622334e4a95ea643a35b5
toxiglinter40 <124887158+toxiglinter40@users.noreply.github.com>
2023-11-28 14:31:54 -0300

    header

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-
+import Header from './components/header/page'
 export default function Home() {
   return (
     <>
+    <Header/>
       <h1>Página home </h1>
     </>
   )
