6cb53aa10c7b2884b1d63968d5eba21c12b63f58
Richard Macedo <richardmacedo.564@gmail.com>
2023-11-28 13:56:00 -0300

    banner pronto

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,9 @@ import styles from './page.module.css';
 
 function App() {
   return (
-    <div className='escuro'>
+    <div >
       <div className={styles.container}>
-      <img src="/banner.jpg" alt="Banner" className={styles.banner} width={1263} height={600}/>
+      <img className={styles.image} src="/banner.jpg" alt="Banner" width={1280} height={500}/>
       </div>
       <div>
       <h1 className={styles.title}>Olá, mundo!</h1>
