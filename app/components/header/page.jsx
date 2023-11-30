778c3dbeb078546467882278c559db5dd1ace17d
rafael amaral <124887158+toxiglinter40@users.noreply.github.com>
2023-11-30 11:48:03 -0300

    troca css

--- app/components/header/page.jsx
+++ app/components/header/page.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <img src="https://mail.google.com/mail/u/0?ui=2&ik=5f1304eb85&attid=0.1&permmsgid=msg-f:1783827309980979023&th=18c16d7cc00e8f4f&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ9RYdsaLq7B5lN4DkSfkfujNGuXFF8OLuMCSF8CxCz-E2UGpMeRKbE3w-6sG_fy9MZU2p_sdubXg_59SYscHvqSsRDwVJY_zX-kjkjhsAEOkISM-lxGjSSkfZk&disp=emb&realattid=ii_lpikrk6c0" alt="" className={styles.img}/>
       </div>
       <div className={styles.div2}>
-        <h1>contato</h1>
+        <button>contato</button>
       </div>
       </div>
     </div>
