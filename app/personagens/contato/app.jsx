01301f7f4f9d409d45d246a5770524e759de106f
toxiglinter40 <124887158+toxiglinter40@users.noreply.github.com>
2023-12-05 07:14:51 -0300

    contato-parte1

---
+++ app/personagens/contato/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+function button (){
+    alert("mensagen enviada!" (app)) 
+
+}
+const app = () => {
+  return (
+    <div>
+      <h1>nome</h1>
+      <input type="text" />
+      <h1>numero</h1>
+      <input type="number" />
+      <h1>email</h1>
+      <input type="email" />
+      <h1>texto</h1>
+      <input type="text" />
+     <button id="buttton" className={Style.button}>enviar</button>
+    </div>
+  )
+}
+
+export default app
