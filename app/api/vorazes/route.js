7bc97db078dbaf60e34f6c219c9101c87d7333f2
Richard Macedo <richardmacedo.564@gmail.com>
2023-12-07 10:20:57 -0300

    vixi

--- app/api/vorazes/route.js
+++ app/api/vorazes/route.js
@@ -4,9 +4,11 @@ import { NextResponse } from "next/server";
 
 const url = process.env.BASE_URL + "/vorazes";
 
-export async function GET() {
+export async function GET(request, { params }) {
+  const { id } = params;
+
   try {
-    const resposta = await axios.get(url);
+    const resposta = await axios.get(`${url}/${id}`);
 
     return NextResponse.json(resposta.data);
   } catch (error) {
@@ -15,15 +17,29 @@ export async function GET() {
   }
 }
 
-export async function POST(request) {
-  const params = await request.json();
+export async function POST(request, { params }) {
+  const { id } = params;
+  const body = await request.json();
 
   try {
-    const resposta = await axios.post(url, params);
+    const resposta = await axios.post(`${url}/${id}`, body);
 
     return NextResponse.json(resposta.data);
   } catch (error) {
     console.log("[ORDER_POST]", error);
+    return new NextResponse("Erro interno do servidor!", { status: 500 });
+  }
+}
+
+export async function DELETE (request, { params }) {
+  const { id } = params;
+
+  try {
+    const resposta = await axios.delete(`${url}/${id}`);
+
+    return NextResponse.json(resposta.data);
+  } catch (error) {
+    console.log("[ORDER_DELETE]", error);
     return new NextResponse("Erro interno do servidor!", { status: 500 });
   }
 }
