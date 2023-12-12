2ee3c7cb23a1f3e9105d1d621be7b56128105a98
sosofria <sophiamarcondes.564@gmail.com>
2023-12-12 16:56:29 -0300

    modalComents

--- app/components/PopUp/popUp.jsx
+++ app/components/PopUp/popUp.jsx
@@ -1,13 +1,17 @@
+//importe dos itens necessarios
 'use client';
 import React, { useState } from 'react';
 import styles from '../PopUp/popUp.module.css'
 import styles from './popUp.module.css'
 
+//definindo o componente Home
 export default function Home() {
+    //definindo o estado inicial para o pop-up
     const [showPopUp, setShowPopUp] = useState(false);
     const [PopupMessage, setPopUpMessage] = useState('');
     const [PopUptype, setPopUptype] = useState('');
 
+    //definindo a função para mostrar o pop-up
     function handleShowPopUp(time, message, type) {
         setPopUptype(type);
         setPopUpMessage(message);
@@ -17,6 +21,7 @@ export default function Home() {
        
         }, time);
    } 
+   //retornando dados
    return (
     <div>
         <button onClick={() => handleShowPopUp('Error', 'error', '4000')}>
