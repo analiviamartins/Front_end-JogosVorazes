d32e1f043cd8d3c3eb21b12570cf8c8520b17d78
sosofria <sophiamarcondes.564@gmail.com>
2023-12-12 10:34:35 -0300

    coments12

--- app/components/vencedor/vencedor.jsx
+++ app/components/vencedor/vencedor.jsx
@@ -1,6 +1,8 @@
+//importe dos itens necessários
 import React from 'react';
 import style from './vencedor.module.css';
 
+//criação do Ganhador
 const Ganhador = ({ isOpen, onClose, winner, player }) => {
     if (!isOpen) {
       return null;
@@ -10,17 +12,22 @@ return (
     <>
      {
         winner ? (
+          //criação do card
             <div className={style.modal_overlay}>
             <div className={style.modal}>
               <div className={style.modal_header}>
+                {/*adição do título*/}
                 <h2>Resultado da Batalha</h2>
+                {/*botão para fechar o card*/}
                 <button className={style.close_button} onClick={onClose}>
                   Fechar
                 </button>
               </div>
               <div className={style.modal_body}>
+                {/*adição do texto*/}
                 <h1>{winner.nome} é o vencedor!</h1>
                 <h2>{player} ganhou desse duelo!</h2>
+                {/*adição da imagem*/}
                 <img src={winner.imagem} alt={winner.nome} width={256}  height={128}/>
                 <p>Ataque: {winner.dano}</p>
                 <p>Defesa: {winner.defesa}</p>
@@ -36,4 +43,5 @@ return (
   );
 };
 
+//exporte da função
 export default Ganhador;
