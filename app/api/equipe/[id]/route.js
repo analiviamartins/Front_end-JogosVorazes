dc7565bea1834a0c85f17a8223c9e5f4a289e7f7
sosofria <sophiamarcondes.564@gmail.com>
2023-12-12 08:38:04 -0300

    comentarios2

--- app/api/equipe/[id]/route.js
+++ app/api/equipe/[id]/route.js
@@ -1,45 +1,54 @@
+//importe dos itens necessários
 import axios from "axios";
 
 import { NextResponse } from "next/server";
 
+//chamada  base url para API
 const url = process.env.BASE_URL  + "/equipe";
 
+//definir função GET
 export async function GET(request, { params }) {
+  //puxar id do parametro
   const { id } = params;
 
+  //fazer uma requisição do get para a API
   try {
     const response = await axios.get(`${url}/${id}`);
 
+    //retornar dados
     return NextResponse.json(response.data);
   } catch (error) {
-    console.log("[ORDER_GET]", error);
     return new NextResponse("Erro interno do servidor!", { status: 500 });
   }
 }
 
+//definir funçao PUT
 export async function PUT(request, { params }) {
+  //puxar id do parametro e do body
   const { id } = params;
-  //console.log(id)
   const body = await request.json();
 
+//fazer uma requisição do PUT
   try {
     const response = await axios.put(`${url}/${id}`, body);
 
+//retornar dados
     return NextResponse.json(response.data);
   } catch (error) {
-    console.log("[ORDER_PUT]", error);
     return new NextResponse("Erro interno do servidor!", { status: 500 });
   }
 }
 
+//definir funçao DELETE
 export async function DELETE(request, { params }) {
+  //puxar id do parametro
   const { id } = params;
   try {
     const response = await axios.delete(`${url}/${id}`);
 
+  //retornar dados
     return NextResponse.json(response.data);
   } catch (error) {
-    console.log("[ORDER_DELETE]", error);
     return new NextResponse("Erro interno do servidor!", { status: 500 });
   }
 }
