f544733ab97b3f10839121ca6ada1a909d5c76fb
sosofria <sophiamarcondes.564@gmail.com>
2023-12-12 08:59:42 -0300

    comentarios5

--- app/api/vorazes/route.js
+++ app/api/vorazes/route.js
@@ -1,29 +1,33 @@
+//importe dos itens necessários
 import axios from "axios"
 
 import { NextResponse } from "next/server";
 
+//chamada base url para API
 const url = process.env.BASE_URL + "/vorazes";
 
+//exporte na função GET
 export async function GET() {
   try {
     const resposta = await axios.get(url);
-
+ 
+  //retornar dados
     return NextResponse.json(resposta.data);
   } catch (error) {
-    console.log("[ORDER_GET]", error);
     return new NextResponse("Erro interno do servidor!", { status: 500 });
   }
 }
 
+//exporte na função POST
 export async function POST(request) {
   const params = await request.json();
 
   try {
     const resposta = await axios.post(url, params);
 
+  //retornar dados
     return NextResponse.json(resposta.data);
   } catch (error) {
-    console.log("[ORDER_POST]", error);
     return new NextResponse("Erro interno do servidor!", { status: 500 });
   }
 }
