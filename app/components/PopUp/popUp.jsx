51bb6fd156841d0b5e7076eea09595f1bd992720
sosofria <sophiamarcondes.564@gmail.com>
2023-12-12 10:27:14 -0300

    coments11

--- app/components/PopUp/popUp.jsx
+++ app/components/PopUp/popUp.jsx
@@ -1,13 +1,16 @@
+//importe dos itens necessários
 'use client';
 import React, { useState } from 'react';
 import styles from '../PopUp/popUp.module.css'
 import styles from './popUp.module.css'
 
+//definir função Home
 export default function Home() {
     const [showPopUp, setShowPopUp] = useState(false);
     const [PopupMessage, setPopUpMessage] = useState('');
     const [PopUptype, setPopUptype] = useState('');
 
+//definir função para mostrar popUp
     function handleShowPopUp(time, message, type) {
         setPopUptype(type);
         setPopUpMessage(message);
