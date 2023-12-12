b90942e52e3736fda8fd878e5ab780b684672785
sosofria <sophiamarcondes.564@gmail.com>
2023-12-12 10:20:13 -0300

    coments10

--- app/components/modal/page.jsx
+++ app/components/modal/page.jsx
@@ -1,8 +1,9 @@
+//importe dos itens necessários
 'use client'
 import React from 'react';
 import style from './modal.module.css';
 
-
+//criação do Modal
 const Modal = ({ isOpen, onClose, vorazes }) => {
   if (!isOpen) {
     return null;
@@ -12,15 +13,19 @@ const Modal = ({ isOpen, onClose, vorazes }) => {
     <>
     {
       vorazes? (
+        //criação do card
       <div className={style.modal_overlay}>
         <div className={style.modal}>
 
           <div className={style.modal_header}>
+            {/*adição da logo*/}
             <img src="/modal.png" alt="Logo" width={70} height={70} className={style.img} />
           </div>
+          {/*adição da descrição*/}
           <div>
             <h2>{vorazes.descricao}</h2>
           </div>
+          {/*botão para fechar o card*/}
           <button className={style.close_button} onClick={onClose}>
               <strong>Fechar</strong>
             </button>
@@ -32,4 +37,5 @@ const Modal = ({ isOpen, onClose, vorazes }) => {
   );
 };
 
+//exporte do Modal
 export default Modal;
