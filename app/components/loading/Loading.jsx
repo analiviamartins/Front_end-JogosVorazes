21dbd2484d05a4d7d0eaefb55cd3de9fdb545b29
sosofria <sophiamarcondes.564@gmail.com>
2023-12-12 10:16:47 -0300

    coments9

--- app/components/loading/Loading.jsx
+++ app/components/loading/Loading.jsx
@@ -1,12 +1,16 @@
+//importe dos itens necessários
 import React from 'react';
 import estilo from './loading.module.css';
 
+//criação do loading
 const Loading = () => {
   return (
     <div className={estilo.full_screen_gif}>
+      {/*adição de um gif*/}
       <img src="/loading.png" alt="GIF" />
     </div>
   );
 };
 
+//exporte do loading
 export default Loading;
